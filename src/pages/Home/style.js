258bc702dddb360f780d026dbeecae441fd48c9a
Fernando Scramignon <scramignonnarde@gmail.com>
2022-11-14 16:29:43 -0300

    feat: creates home page

---
+++ src/pages/Home/style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+    
+    & {
+        background: var(--grey-0);
+        min-height: 100vh;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+`
